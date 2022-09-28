e801a7703531f9b9f8d6f03a2f9e7d5d2bde84c8
walber-fac <74104683+walber-fac@users.noreply.github.com>
2022-09-27 22:16:30 -0300

    Primeiro prototipo

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 import pygame
-from pygame.locals import *
-from sys import exit 
+from sys import exit # permits the exit work
 
 pygame.init()
+#opens the screen and define the size width x height
+screen = pygame.display.set_mode((510,510))
+#displays the name of the software:
+pygame.display.set_caption("Monopoly")
+clock = pygame.time.Clock() # to be abe to define the frame rate
 
-largura = 640
-altura = 480
-tela = pygame.display.set_mode((largura, altura))
+test_surface = pygame.image.load("Pictures\Template\Template1.png")
+#test_surface = pygame.image.load()#creates surface and define size
+#test_surface.fill("Red")
 
 while True:
-    for evento in pygame.event.get():
-        if evento.type == QUIT:
-            pygame.quit()
-            exit()
-    pygame.display.update() 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()#the x box can quit the screen
+            exit()#exit stops the loop while True
+
+    #put one surface on another surface
+    screen.blit(test_surface, (0,0))#block image transfer, pass argument and set the position 
+    
+
+    pygame.display.update()#keep the screen running
+    clock.tick(20) #defines the maximum frame rate of while True
