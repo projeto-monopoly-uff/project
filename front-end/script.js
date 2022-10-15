a17ef84527d976aa5db102752ff50e01c1b4f061
Joel Lopes <joellopessouza@id.uff.br>
2022-10-15 15:54:57 -0300

    Comentando o script.js

--- front-end/script.js
+++ front-end/script.js
@@ -1,8 +1,7 @@
-/*$(document).ready(function(){
-    for(i = 1; i++; i <= 11){
-        //$("insert-here").append(`<div class="grid-item first">${i}</div>`);
-    }
-}) */
+/* 
+Iteração pra criar um tabuleiro, semelhante a uma matriz, mas o centro tem apenas UM  elemento que
+no CSS é configurado pra ocupar todo o centro da tabela, no caso o centro grandão do tabuleiro.  
+*/
 for(var i = 1; i < 12; i++){
     for(var j = 1; j < 12; j++){
         if(i == 1 || i == 11){
